076928698e3fa53aba45ece87a381333e720f635
Dimitar Danailov <dimityr.danailov@gmail.com>
2018-10-06 16:39:17 +0300

    Fix go imports.

--- cmd/gosofia/main.go
+++ cmd/gosofia/main.go
@@ -10,7 +10,7 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/dimitardanailov/SofiaGoWorkshop/internal/diagnostics"
+	"github.com/dimitardanailov/sofiagoworkshop/internal/diagnostics"
 
 	"github.com/gorilla/mux"
 )
