8353662bc372373a4ab88afe8b986c0621d20509
Dimitar Danailov <dimityr.danailov@gmail.com>
2018-10-06 11:49:30 +0300

    I've completed two parts - Hello world and http server.

---
+++ cmd/gosofia/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	""
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	log.Printf("Hello, World")
+
+	router := mux.NewRouter()
+	router.HandleFunc("/", hello)
+
+	go func() {
+		err := http.ListenAndServe(":8080", router)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	diagnostics := diagnostics.NewDiagnostics()
+	err := http.ListenAndServe(":8585", diagnostics)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, http.StatusText(http.StatusOK))
+}
