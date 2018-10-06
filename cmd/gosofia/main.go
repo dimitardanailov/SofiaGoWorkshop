5de85d1edad841a0f48ddb6683d5ee196b62a2f4
Dimitar Danailov <dimityr.danailov@gmail.com>
2018-10-06 12:52:26 +0300

    Repo has a structure which is responsible about server configurations.

--- cmd/gosofia/main.go
+++ cmd/gosofia/main.go
@@ -1,35 +1,79 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
-	""
+	"github.com/dimitardanailov/SofiaGoWorkshop/internal/diagnostics"
 
 	"github.com/gorilla/mux"
 )
 
+type serverConf struct {
+	port   string
+	router http.Handler
+	name   string
+}
+
 func main() {
-	log.Printf("Hello, World")
+	log.Printf("Starting the application ...")
+
+	blPort := os.Getenv("PORT")
+	if len(blPort) == 0 {
+		log.Fatal("The application port should be set")
+	}
+
+	diagPort := os.Getenv("DIAG_PORT")
+	if len(diagPort) == 0 {
+		log.Fatal("The health checker port should be set")
+	}
 
 	router := mux.NewRouter()
 	router.HandleFunc("/", hello)
 
-	go func() {
-		err := http.ListenAndServe(":8080", router)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}()
+	configurations := []serverConf{
+		{
+			port:   blPort,
+			router: router,
+			name:   "application server",
+		},
+		{
+			port:   diagPort,
+			router: diagnostics.NewDiagnostics(),
+			name:   "diagnostics server",
+		},
+	}
 
-	diagnostics := diagnostics.NewDiagnostics()
-	err := http.ListenAndServe(":8585", diagnostics)
-	if err != nil {
+	possibleErrors := make(chan error, 2)
+	servers := make([]*http.Server, 2)
+
+	for i, c := range configurations {
+		go func(conf serverConf, i int) {
+			log.Printf("The %s is preparing to handle connections...", conf.name)
+			servers[i] = &http.Server{
+				Addr:    ":" + conf.port,
+				Handler: conf.router,
+			}
+			err := servers[i].ListenAndServe()
+			if err != nil {
+				possibleErrors <- err
+			}
+		}(c, i)
+	}
+
+	select {
+	case err := <-possibleErrors:
+		for _, s := range servers {
+			s.Shutdown(context.Background())
+		}
 		log.Fatal(err)
 	}
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
+	log.Printf("The application is server is ready to handle connection ...")
 	fmt.Fprint(w, http.StatusText(http.StatusOK))
 }
---
+++ go.mod
@@ -0,0 +1,6 @@
+github.com/dimitardanailov/SofiaGoWorkshop
+
+require (
+	github.com/gorilla/context v1.1.1 // indirect
+	github.com/gorilla/mux v1.6.2
+)
